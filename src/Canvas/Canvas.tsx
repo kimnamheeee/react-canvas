e6d4c4c3612927a4d53847375594d85ce01c95ea
kimnamheeee <nami200284@snu.ac.kr>
2024-11-03 13:53:57 +0900

    feat: simple animation

--- src/Canvas/Canvas.tsx
+++ src/Canvas/Canvas.tsx
@@ -3,17 +3,30 @@ import { useRef, useEffect } from "react"
 export default function Canvas({ ... props }) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
-    const draw = (ctx: CanvasRenderingContext2D) => {
+    const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         ctx.fillStyle = 'grey';
-        ctx.fillRect(10, 10, 100, 100);
+        const delta = frameCount % 800;
+        ctx.fillRect(10 + delta, 10, 100, 100);
     }
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas?.getContext('2d');
 
-        if (context) {
-            draw(context);
+        let animationFrameId: number;
+        let frameCount = 0;
+
+        const render = () => {
+            frameCount++;
+            draw(context!, frameCount);
+            animationFrameId = window.requestAnimationFrame(render);
+        }
+
+        render();
+
+        return () => {
+            window.cancelAnimationFrame(animationFrameId);
         }
     }, []);
 
