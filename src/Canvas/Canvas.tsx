9a4aa2d1423074130411ac17e4f320dc9f6ee417
kimnamheeee <nami200284@snu.ac.kr>
2024-11-03 13:38:26 +0900

    refactor: add draw function

--- src/Canvas/Canvas.tsx
+++ src/Canvas/Canvas.tsx
@@ -3,13 +3,17 @@ import { useRef, useEffect } from "react"
 export default function Canvas({ ... props }) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
+    const draw = (ctx: CanvasRenderingContext2D) => {
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(10, 10, 100, 100);
+    }
+
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas?.getContext('2d');
 
         if (context) {
-            context.fillStyle = 'grey';
-            context.fillRect(10, 10, 100, 100);
+            draw(context);
         }
     }, []);
 
