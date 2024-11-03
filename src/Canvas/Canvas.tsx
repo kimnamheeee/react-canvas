e780f0021014c18770f193442b5af7f31f98c77f
kimnamheeee <nami200284@snu.ac.kr>
2024-11-03 13:31:16 +0900

    feat: draw simple rect on canvas

--- src/Canvas/Canvas.tsx
+++ src/Canvas/Canvas.tsx
@@ -1,7 +1,17 @@
-import { useRef } from "react"
+import { useRef, useEffect } from "react"
 
 export default function Canvas({ ... props }) {
-    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas?.getContext('2d');
+
+        if (context) {
+            context.fillStyle = 'grey';
+            context.fillRect(10, 10, 100, 100);
+        }
+    }, []);
 
     return (
         <canvas ref={canvasRef} {...props} />
